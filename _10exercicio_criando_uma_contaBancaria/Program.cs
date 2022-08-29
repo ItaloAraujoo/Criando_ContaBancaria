e7e98e4d695dad0fa81128f37e70a7ae36d01176
ItaloAraujoo <italo3040@gmail.com>
2022-08-29 11:29:58 -0300

    Add project files.

---
+++ _10exercicio_criando_uma_contaBancaria/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace conta_bancaria
+{
+    using System.Globalization;
+    using _10exercicio_criando_uma_contaBancaria;
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBancaria conta;
+
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome do titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial s / n ? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S'){
+                Console.Write("Entre com o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
